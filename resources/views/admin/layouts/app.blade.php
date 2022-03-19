22107e20cd5e70d05de587bec0195445046a699b
unknown <william.w.rocha@gmail.com>
2022-03-18 21:01:38 -0300

    faltando alguns estilos

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }}</title>
+
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
-<body>
-    
-    <div class="content">
+<body class="bg-gray-50">
+
+    <div class="container mx-auto py-8">
         @yield('content')
     </div>
 
